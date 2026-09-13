e7f5849f952d3b4ae981bb869a5dd50d85499517
techzoneadapter-droid <techzone.adapter@gmail.com>
2026-09-13 22:36:27 +0700

    Switch app to EZTOOL-inspired dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import BmTokenCreatorHub from './bm-token-creator-hub';
 import CopyPolish from './copy-polish';
 import CrmResourcePush from './crm-resource-push';
 import EncodingRepair from './encoding-repair';
+import EztoolDashboard from './eztool-dashboard';
 import GuideWorkspaceSection from './guide-workspace-section';
 import MetaOAuthConnector from './meta-oauth-connector';
 import ProductionQueue from './production-queue';
 import ResourceCenter from './resource-center';
 import ResourcePresetManager from './resource-preset-manager';
 import TokenWorkspaceSection from './token-workspace-section';
-import Workspace from './workspace';
 import WorkflowRunner from './workflow-runner';
 
 export default function Page() {
@@ -17,7 +17,7 @@ export default function Page() {
     <>
       <EncodingRepair />
       <CopyPolish />
-      <Workspace />
+      <EztoolDashboard />
       <BmTokenCreatorHub />
       <ResourceCenter />
       <BulkHealthCheck />
