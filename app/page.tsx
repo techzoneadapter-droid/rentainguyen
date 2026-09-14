dc391ca7ce1057f1940e6878d06122b46ce7a669
techzoneadapter-droid <techzone.adapter@gmail.com>
2026-09-14 07:41:22 +0700

    Switch app to rebuilt EZTOOL-style shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,5 @@
-import BulkHealthCheck from './bulk-health-check';
-import BulkTokenCenter from './bulk-token-center';
-import BmTokenCreatorHub from './bm-token-creator-hub';
-import CopyPolish from './copy-polish';
-import CrmResourcePush from './crm-resource-push';
-import EncodingRepair from './encoding-repair';
-import EztoolDashboard from './eztool-dashboard';
-import GuideWorkspaceSection from './guide-workspace-section';
-import MetaOAuthConnector from './meta-oauth-connector';
-import ProductionQueue from './production-queue';
-import ResourceCenter from './resource-center';
-import ResourcePresetManager from './resource-preset-manager';
-import TokenImportCompat from './token-import-compat';
-import TokenWorkspaceSection from './token-workspace-section';
-import WorkflowRunner from './workflow-runner';
+import EztoolCloneDashboard from './eztool-clone-dashboard';
 
 export default function Page() {
-  return (
-    <>
-      <EncodingRepair />
-      <CopyPolish />
-      <EztoolDashboard />
-      <BulkTokenCenter />
-      <TokenImportCompat />
-      <TokenWorkspaceSection />
-      <BmTokenCreatorHub />
-      <ResourceCenter />
-      <BulkHealthCheck />
-      <ProductionQueue />
-      <ResourcePresetManager />
-      <GuideWorkspaceSection />
-      <MetaOAuthConnector />
-      <CrmResourcePush />
-      <WorkflowRunner />
-    </>
-  );
+  return <EztoolCloneDashboard />;
 }
