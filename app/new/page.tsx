f0901c074012513543b4e23a6865bc71040bd25b
techzoneadapter-droid <techzone.adapter@gmail.com>
2026-09-14 08:34:29 +0700

    Mount existing tool logic in new dashboard

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,5 +1,11 @@
 import EztoolCloneDashboard from '../eztool-clone-dashboard';
+import NewToolBridge from '../new-tool-bridge';
 
 export default function NewDashboardPage() {
-  return <EztoolCloneDashboard />;
+  return (
+    <>
+      <EztoolCloneDashboard />
+      <NewToolBridge />
+    </>
+  );
 }
